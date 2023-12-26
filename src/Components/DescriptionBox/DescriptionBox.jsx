7e56d94065943b0fd2d88df7a93d1c06c730ac8b
lakshayletsgo <121108594+lakshayletsgo@users.noreply.github.com>
2023-12-27 01:53:10 +0530

    ShopZone

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './DescriptionBox.css'
+export default function DescriptionBox() {
+  return (
+    <div className='descriptionBox'>
+        <div className="descriptionBox-navigator">
+            <div className="descriptionBox-nav-box">
+                Description
+            </div>
+            <div className="descriptionBox-nav-box fade">
+                Reviews (122)
+            </div>
+            </div>
+            <div className="descriptionBox-description">
+                <p>
+                    This is sample website created by lakshay. It is just a learning website that is created for entertainment. To check more projects of me you can go to my github id --{'>'} lakshayletsgo. This website has gained very popularity. 
+                    This website took me 12 hr to make. I have made so that i can relearn my web developnment concepts. 
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Error distinctio vero repellat dolore incidunt perferendis enim id necessitatibus libero vitae consectetur, nulla qui fugiat reiciendis sapiente eaque laboriosam blanditiis ad earum accusamus! Atque culpa suscipit velit adipisci, ea non necessitatibus nesciunt amet temporibus autem, accusantium, voluptatibus magnam ad quod. Ut omnis ad eius laborum, quia aspernatur placeat expedita necessitatibus ea.
+                </p>
+        </div>
+    </div>
+  )
+}
